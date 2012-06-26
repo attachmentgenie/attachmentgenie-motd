a08e400d7156f40028ec95596f6913e52b960030
bram vogelaar <bram@attachmentgenie.com>
2012-06-26 11:39:03 +0200

    moving define register to register.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -24,21 +24,7 @@ class motd {
 
   concat::fragment{'motd_header':
     target  => $motd,
-    content => "\nPuppet modules on this server:\n\n",
+    content => '\nPuppet modules on this server:\n\n',
     order   => 01,
-  }
-}
-
-# used by other modules to register themselves in the motd
-define motd::register($content='', $order=10) {
-  if $content == '' {
-    $body = $name
-  } else {
-    $body = $content
-  }
-
-  concat::fragment{'motd_fragment_$name':
-    target  => "/etc/motd.tail",
-    content => "    -- $body\n",
   }
 }
