cac23d9f9af9391e1e74a6c839dc55c5781e7bcf
bram vogelaar <bram@attachmentgenie.com>
2012-06-26 09:39:31 +0200

    initial import

---
+++ Modulefile
@@ -0,0 +1,11 @@
+name    'bram-attachmentgenie-motd'
+version ''
+source ''
+author 'bram'
+license ''
+summary ''
+description ''
+project_page ''
+
+## Add dependencies, if any:
+# dependency 'username/name', '>= 1.2.0'
---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: attachmentgenie-motd
+#
+# This module manages attachmentgenie-motd
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class attachmentgenie-motd {
+
+
+}
